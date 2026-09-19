c0b808436ff742d17d1ce4b7d8945aa0c7e33f1c
snaykeder3-hue <snaykeder3@gmail.com>
2026-09-20 01:09:41 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application") version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "1.9.24"
+}
+
+android {
+    namespace = "com.example.irbulucu"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.irbulucu"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
